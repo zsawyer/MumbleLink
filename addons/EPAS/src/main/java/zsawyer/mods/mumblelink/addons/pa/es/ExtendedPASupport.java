447769171aba151e1d4ce2c3faacfbe0d53a49e6
zsawyer <sawyer.zackery@gmail.com>
2014-12-14 05:16:32 +0100

    * switched to java version 1.6 * fixed dependency declarations between core and addon

--- addons/EPAS/src/main/java/zsawyer/mods/mumblelink/addons/pa/es/ExtendedPASupport.java
+++ addons/EPAS/src/main/java/zsawyer/mods/mumblelink/addons/pa/es/ExtendedPASupport.java
@@ -22,28 +22,7 @@
 
 package zsawyer.mods.mumblelink.addons.pa.es;
 
-/*
- mod_MumbleLink - Positional Audio Communication for Minecraft with Mumble
- Copyright 2011-2013 zsawyer (http://sourceforge.net/users/zsawyer)
-
- This file is part of mod_MumbleLink
- (http://sourceforge.net/projects/modmumblelink/).
-
- mod_MumbleLink is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- mod_MumbleLink is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with mod_MumbleLink.  If not, see <http://www.gnu.org/licenses/>.
-
- */
-
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
@@ -53,7 +32,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
 import org.apache.logging.log4j.Logger;
 import zsawyer.mods.mumblelink.api.Activateable;
 import zsawyer.mods.mumblelink.api.IdentityManipulator;
