cd6be87c4a4d05f2899e182629b3349a513f0506
Arthur <arthurdanjou@outlook.fr>
2020-04-20 15:36:44 +0200

    Fix version

--- mod/src/main/java/zsawyer/mods/mumblelink/MumbleLinkImpl.java
+++ mod/src/main/java/zsawyer/mods/mumblelink/MumbleLinkImpl.java
@@ -24,7 +24,6 @@ package zsawyer.mods.mumblelink;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
